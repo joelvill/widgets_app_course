5941439cc70be34d411d00b11743a105c5a65ca9
host <taijomi17@gmail.com>
2024-07-08 17:59:45 -0400

    screens creadas

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScrenn extends StatelessWidget {
+  static const name = '/app_tutorial_screen';
+
+  const AppTutorialScrenn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
